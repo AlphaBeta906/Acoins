9c255d71b6a3053f6fb6811d1b25fbc2cf460f91
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-06-09 19:09:30 +0800

    Add files via upload

---
+++ money.py
@@ -0,0 +1,32 @@
+from random import randint
+from time import sleep
+money = 0
+while True:
+    c = randint(1, 2)
+    if c == 1:
+        people = "Miner " + str(randint(1, 9999999))
+    else:
+        people = "User " + str(randint(1, 9999999))
+    chance = randint(1, 99999990)
+    if chance == 1:
+        resiver = "You"
+        money_lost = randint(1, 10000)
+        money = money_lost + money
+    else:
+        cu = randint(1, 2)
+        if cu == 1:
+            resiver = "Miner " + str(randint(1, 9999999))
+        else:
+            resiver = "User " + str(randint(1, 9999999))
+        money_lost = randint(1, 99)
+    if c == 1:
+        co = randint(1, 2)
+        if co == 1:
+            say = people + " gets " + str(money_lost) + " acoins"
+            money += money_lost
+        else:
+            say = people + " pays " + str(money_lost) + " acoins to " + resiver
+    else:
+        say = people + " pays " + str(money_lost) + " acoins to " + resiver
+    print (say)
+    sleep(1)
